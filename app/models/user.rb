5bc88f99b434167b1e1f6ce9aceeffb5ce7fef24
DrCool2 <drcoolnow@gmail.com>
2020-08-25 15:43:32 -0400

    created a User Full_Name method AND got Entries New to work with User and Task.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  has_many :entries, dependent: :destroy 
+  has_many :entries, dependent: :destroy
+
+  def full_name(id)
+     # User.all.each do |u|
+     #   return [[u.id],[u.first_name + " " + u.last_name]]
+     # end
+     user_id = User.find_by_id(id)
+     return user_id.first_name + " " + user_id.last_name
+  end
 end
